e840b1c2758d6b1ac6839c0e470342ccaa89e328
Matthew Earl <matthew.earl@gmail.com>
2014-06-02 16:32:47 +0100

    Add q3/__init__.py and test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+import q3.bsp
+import q3.fs
+
+
+fs = q3.fs.FileSystem.from_dir("../q3data/")
+
+with fs.open("maps/q3dm7.bsp") as bsp_file:
+    bsp = q3.bsp.Bsp(bsp_file)
+
+    pprint(bsp.faces)
+
