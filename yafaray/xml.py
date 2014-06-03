0a3448be84662333efbb24f742a4b27a598ed2c8
Matthew Earl <matthew.earl@gmail.com>
2014-06-03 09:10:50 +0100

    More yafaray coding

--- yafaray/xml.py
+++ yafaray/xml.py
@@ -3,7 +3,6 @@ __all__ = (
     'write',
 )
 
-
 class _Tag():
     def __init__(self, name, *, **params):
         self.name = name
@@ -76,17 +75,74 @@ class _XmlWriter():
                                            g=light.color[1],
                                            b=light.color[2]))
 
+    def _write_camera(self):
+        with self._in_tag(_Tag("camera", name="cam")):
+            self._output_line(_Tag("type", sval="perspective"))
+            self._output_line(_Tag("up", x=0, y=1, z=0))
+            self._output_line(_Tag("from",
+                                   x=self._scene.camera.location[0],
+                                   y=self._scene.camera.location[1],
+                                   z=self._scene.camera.location[2]))
+            self._output_line(_Tag("look_at",
+                                   x=self._scene.camera.look_at[0],
+                                   y=self._scene.camera.look_at[1],
+                                   z=self._scene.camera.look_at[2]))
+    def _write_render(self):
+        self._xml_file.write(""" 
+<background name="world_background">
+	<color r="0.437557" g="0.546283" b="1" a="1"/>
+	<power fval="1"/>
+	<type sval="constant"/>
+</background>
 
+<integrator name="default">
+	<bounces ival="3"/>
+	<caustic_mix ival="5"/>
+	<diffuseRadius fval="1"/>
+	<fg_bounces ival="3"/>
+	<fg_samples ival="32"/>
+	<finalGather bval="true"/>
+	<photons ival="200000"/>
+	<raydepth ival="4"/>
+	<search ival="150"/>
+	<shadowDepth ival="2"/>
+	<show_map bval="false"/>
+	<transpShad bval="false"/>
+	<type sval="photonmapping"/>
+	<use_background bval="false"/>
+</integrator>
 
+<integrator name="volintegr">
+	<type sval="none"/>
+</integrator>
+
+<render>
+	<AA_inc_samples ival="2"/>
+	<AA_minsamples ival="4"/>
+	<AA_passes ival="2"/>
+	<AA_pixelwidth fval="1.5"/>
+	<AA_threshold fval="0.05"/>
+	<background_name sval="world_background"/>
+	<camera_name sval="cam"/>
+	<clamp_rgb bval="true"/>
+	<filter_type sval="mitchell"/>
+	<gamma fval="2.2"/>
+	<height ival="375"/>
+	<integrator_name sval="default"/>
+	<threads ival="1"/>
+	<volintegrator_name sval="volintegr"/>
+	<width ival="375"/>
+	<xstart ival="0"/>
+	<ystart ival="0"/>
+	<z_channel bval="true"/>
+</render>
+"""
+            
     def _write(self):
-
-        self._write_mesh()
-        self._write_camera()
-        self._write_render()
-        self._write_lights()
-
-
-
-
+        with self._in_tag(Tag("scene")):
+            self._write_camera()
+            self._write_lights()
+            self._write_render()
+            self._write_mesh()
 
 def write(xml_file, scene):
