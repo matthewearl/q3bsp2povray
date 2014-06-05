f9bcf6378e2baf175b1ea04db0f1560ea9f33eee
Matthew Earl <matthew.earl@gmail.com>
2014-06-05 08:18:23 +0100

    Actual initial commit of loadcolors ?!

--- loadcolors.py
+++ loadcolors.py
@@ -17,19 +17,52 @@ __all__ = (
     'main'
 )
 
-import PIL
+from PIL import Image
 
 
-def calculate_color(fs, tex_name):
+def _open_tex_file(fs, tex_name):
     def match(path):
         return path.lower() in (
                 tex_name.lower() + ".jpg", tex_name.lower() + ".tga")
 
     match_iter = iter(path for path in fs.paths if match(path))
     try:
-        img_path = next(match_iter)
+        tex_path = next(match_iter)
     except StopIteration:
-        raise 
+        raise KeyError("Texture {} not found within FS".format(tex_name))
+
+    return fs.open(tex_path)
+
+
+def calculate_color(fs, tex_name):
+    """
+    Calculate the average color of a texture.
+
+    Arguments:
+        fs: Filesystem to find the texture.
+        tex_name: Name of the texture to find (without extension).
+
+    Returns:
+        An RGB triple representing the average color.
+
+    """
+    tex_file = _open_tex_file(fs, tex_name)
+    
+    im = Image.open(tex_file)
+
+    # This seems like an efficient method in terms of offloading as much work
+    # as possible to C: Calculate the histogram, and then sum the respective
+    # components. Finally, each sum by the number of pixels to obtain the
+    # average.
+
+    im = im.convert("RGB")
+    hist = im.histogram()
+    def _component_average(comp_hist):
+        return float(sum(val * count
+                    for val, count in enumerate(comp_hist))) / (
+                                256. * im.size[0] * im.size[1])
+    return tuple(_component_average(hist[(256 * c):(256 * (c + 1))])
+                    for c in range(3))
 
 
 def _parse_args(in_args):
@@ -42,6 +75,8 @@ def _parse_args(in_args):
 
     return parser.parse_args(in_args)
 
+def _color_to_hex(color):
+    return "#{}".format("".join("%02X" % int(255. * c) for c in color))
 
 def main(argv):
     args = _parse_args(argv)
@@ -49,9 +84,11 @@ def main(argv):
     with fs.open("maps/{}.bsp".format(args.map)) as bsp_file:
         bsp = q3.bsp.Bsp(bsp_file)
         for tex_name in (tex.name for tex in bsp.textures):
-            print("{} {}".format(
-                calculate_color(fs, tex_name),
-                name))
+            color = calculate_color(fs, tex_name),
+            print('<span style="background-color:{};width:40;float:left;">'
+                    .format(_color_to_hex(color)))
+            print('x</span>')
+            print("<p>{} {}</p>".format(color, name))
 
 
 if __name__ == "__main__":
