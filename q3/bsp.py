0f4c8dc2a568a75cfe3b31fd0cd4fffd21cbad70
Matthew Earl <matthew.earl@gmail.com>
2014-06-05 07:33:03 +0100

    Add support for Texture lump

--- q3/bsp.py
+++ q3/bsp.py
@@ -24,6 +24,9 @@ Vert = collections.namedtuple('Vert',
     ['x', 'y', 'z',
     ])
 
+Texture = collections.namedtuple('Texture',
+    ['name', 'flags', 'contents'])
+
 class _LumpEnum:
     ENTITIES = 0
     TEXTURES = 1
@@ -192,8 +195,12 @@ class _FaceLump(_StructLump):
         n_vertexes = unpacked[4]
         meshvert = unpacked[5]
         n_meshverts = unpacked[6]
+        patch_size = (unpacked[24], unpacked[25])
 
         if face_type in (_FaceType.POLYGON, _FaceType.MESH): 
+            # `vertex` and `n_vertex` describe the vertices of the mesh/poly.
+            # `meshverts` and `n_meshverts` describe the triangulation of these
+            # verts.
             vert_indices = range(vertex, vertex + n_vertexes)
             verts = [self._bsp.verts[i] for i in vert_indices]
 
@@ -202,8 +209,31 @@ class _FaceLump(_StructLump):
                 self._bsp.faces.append(
                         Face(verts=[verts[self._bsp.meshverts[idx + i]]
                                         for i in range(3)]))
+        if face_type == _FaceType.PATCH:
+            # `vertex` and `n_vertex` describe the control points of the patch.
+            # The control points are a grid of size `patch_size`.
+            assert patch_size[0] * patch_size[1] == n_vertexes
+
+            verts = { (i, j):
+                         self._bsp.verts[vertex + i + j * patch_size[0]]
+                            for j in range(patch_size[1])
+                                for i in range(patch_size[0])
+                    }
+
+            # No interpolation yet, just triangulate the control points.
+            for j in range(patch_size[1] - 1):
+                for i in range(patch_size[0] - 1):
+                    self._bsp.faces.append(
+                        Face(verts=[verts[i, j],
+                                    verts[i + 1, j],
+                                    verts[i + 1, j + 1]]))
+                    self._bsp.faces.append(
+                        Face(verts=[verts[i, j],
+                                    verts[i + 1, j + 1],
+                                    verts[i, j + 1]]))
         else:
             pass
+
 
 @_lump_class(_LumpEnum.ENTITIES)
 class _EntitiesLump(_Lump):
@@ -217,6 +247,29 @@ class _EntitiesLump(_Lump):
         ents_str = self._bsp_file.read(self._length).decode('ascii')
         self._bsp.entities = ents.parse(ents_str)
 
+
+@_lump_class(_LumpEnum.TEXTURES)
+class _TextureLump(_StructLump):
+    """
+    Please see http://www.mralligator.com/q3/#Textures for details of this
+    lump.
+
+    """
+
+    _struct_fmt = "<64sII"
+
+    def _start_lump(self):
+        self._bsp.textures = []
+
+    def _read_from_unpacked(self, unpacked): 
+        name = unpacked[0].decode('ascii')
+        null_term_idx = name.find('\x00')
+        if null_term_idx != -1:
+            name = name[:null_term_idx]
+        self._bsp.textures.append(Texture(
+            name=name,
+            flags=unpacked[1],
+            contents=unpacked[2]))
 
 class _SeekableFile():
     """
@@ -275,6 +328,7 @@ class Bsp():
             for lump_num, cls in _lump_classes.items()
         }
 
+        _lump_readers[_LumpEnum.TEXTURES]._read()
         _lump_readers[_LumpEnum.VERTEXES]._read()
         _lump_readers[_LumpEnum.MESHVERTS]._read()
         _lump_readers[_LumpEnum.FACES]._read()
